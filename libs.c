86b270c342db5d4253e61bdf4429f29d9f2529f5
Jonw27 <18wongj@gmail.com>
2017-12-02 17:27:57 -0500

    work from previous stuff. will do semicolon separation

--- libs.c
+++ libs.c
@@ -4,19 +4,19 @@ int specialCase(char * arg, char * arg2){
   if(arg == NULL){
     return 0;
   }
-  if(!strncmp(arg, "cd", sizeof(*arg))){
+  if(strncmp(arg, "cd", sizeof(*arg)) == 0){
     ret = 1;
   }
-  else if(!strncmp(arg, "exit", sizeof(*arg))){
+  else if(strncmp(arg, "exit", sizeof(*arg)) == 0){
     ret = 2;
   }
-  else if(!strncmp(arg, "setngs", sizeof(*arg))){
+  else if(strncmp(arg, "setngs", sizeof(*arg)) == 0){
     ret = 3;
   }
   else if(arg2 == NULL){
     return 0;
   }
-  if(!strncmp(arg, "man", sizeof(*arg)) && !strncmp(arg2, "sssh", sizeof(*arg2))){
+  if(strncmp(arg, "man", sizeof(*arg)) == 0 && strncmp(arg2, "sssh", sizeof(*arg2)) == 0){
     ret = 4;
   }
   return ret;
--- main.c
+++ main.c
@@ -25,25 +25,58 @@ char ** parse_args(char line[]){
   return args;
 }
 
+char ** parse_cmds(char line[]){
+  char ** args;
+  args = malloc(100); // support 100 args
+  int i = 0;
+  char * element;
+  element = malloc(100); // support 100 args
+  while((element = strsep(&line, ";")) != NULL){
+    args[i] = element;
+    i++;
+  }
+  args[i] = NULL;
+  return args;
+}
+ 
+
+void print_args(char ** args){
+	int i = 0;
+	while(args[i]){
+		printf("args[%d]: %s\n", i, args[i]);
+		i++;
+	}
+}
+
+void run_cmd(char * cmd){
+	
+}
+
 int main(){
   signal(SIGINT, sighandler);
   char line[200]; // can change size of this later
   while(1){
     prompt();
     fgets(line, 200, stdin);
+    line[strcspn(line, "\n")] = 0;
     printf("\x1b[0m \n");
+    char ** cmds = parse_cmds(line);
+    // inlude separate command, runcmd
     char ** args = parse_args(line);
+    print_args(args);
     int class = specialCase(args[0], args[1]); // cd, exit, my shell man page
-    if(!class){
+    printf("class: %d\n", class);
+    int status;
+    if(1){//!class){
       int f = fork();
-      int * status;
       if(f == 0){
+	printf("forked\n");
         execvp(args[0], args);
       }
       else{
-        int no_err = wait(status);
-        if(!no_err)
-          printf("Command could not be completed.\n");
+        int no_err = wait(&status);
+        //printf("child status: %d", WEXITSTATUS(status));
+	//printf("parent finished");
       }
     }
     else{
